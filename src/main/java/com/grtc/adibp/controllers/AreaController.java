1ad90a94b697b067d4cb1404d4b5e647efb0bbfa
Eddie paz reyes <eddie230@hotmail.com>
2024-05-12 00:11:56 -0500

    delete: delete importations obsolete security

--- src/main/java/com/grtc/adibp/controllers/AreaController.java
+++ src/main/java/com/grtc/adibp/controllers/AreaController.java
@@ -2,7 +2,6 @@ package com.grtc.adibp.controllers;
 
 import com.grtc.adibp.entities.AreaEntity;
 import com.grtc.adibp.services.AreaServiceImpl;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/com/grtc/adibp/controllers/EmpleadoController.java
+++ src/main/java/com/grtc/adibp/controllers/EmpleadoController.java
@@ -2,7 +2,6 @@ package com.grtc.adibp.controllers;
 
 import com.grtc.adibp.entities.EmpleadoEntity;
 import com.grtc.adibp.services.EmpleadoServiceImpl;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
